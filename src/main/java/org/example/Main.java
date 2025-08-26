cc52fde420a22ab6f3af27bec9868e239857401e
ashishgole30 <ashish.gole3010@gmail.com>
2025-08-26 18:32:00 +0530

    changes to code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,30 +7,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // System.out.println("Hello World ");
-        // Making the above line a comment since we dont need it in the actual project
+        String choice = "y";
 
-        // Asking For User input of Date , Category , Amount and Description
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("WELCOME TO EXPENSE TRACKER");
+        while (choice.equalsIgnoreCase("y")) {
+            System.out.println("Welcome To Expense Tracker");
 
-        System.out.println("Enter Date (YYYY-MM-DD):");
-        String date = scanner.nextLine();
+            System.out.println("Enter Date (YYYY-MM-DD):");
+            String date = scanner.nextLine();
 
-        System.out.println("Enter Category:");
-        String category = scanner.nextLine();
+            System.out.println("Enter Category (Food, Transport, Utilities,Rent etc.):");
+            String category = scanner.nextLine();
 
-        System.out.println("Enter Amount:");
-        double amount = scanner.nextDouble();
-        scanner.nextLine();
+            System.out.println("Enter Amount:");
+            double amount = scanner.nextDouble();
+            scanner.nextLine();
 
-        System.out.println("Enter Description:");
-        String description = scanner.nextLine();
+            System.out.println("Enter Description:");
+            String description = scanner.nextLine();
 
-        // HOW TO STORE THIS USER INPUT IN SOMETHING ELSE ? OTHER THAN VARIABLES ?
+            System.out.println("Do you want to add another expense? (y/n)");
+            choice = scanner.nextLine();
 
-        // HOW TO STORE IT IN A MAP?
+        }
+
+        System.out.println("Thank you for using Expense Tracker!");
 
     }
+
 }
